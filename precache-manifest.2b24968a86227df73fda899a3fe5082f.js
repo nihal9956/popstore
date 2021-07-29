ee39bc060f5d372ed674dbefa7be8fe4f7913194
nihaltiwari <nihaltiwari28@gmail.com>
2021-07-29 12:22:46 +0530

    Updates

---
+++ precache-manifest.2b24968a86227df73fda899a3fe5082f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648dab5d2402539b552698c988c52238",
+    "url": "/popstore/index.html"
+  },
+  {
+    "revision": "ba930e71f1e70fc71875",
+    "url": "/popstore/static/css/2.5ded8190.chunk.css"
+  },
+  {
+    "revision": "ba930e71f1e70fc71875",
+    "url": "/popstore/static/js/2.2ed64952.chunk.js"
+  },
+  {
+    "revision": "068b8ce39271ecaddef8872b712f8973",
+    "url": "/popstore/static/js/2.2ed64952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc0437d059f9b8295bd8",
+    "url": "/popstore/static/js/main.0a937ab8.chunk.js"
+  },
+  {
+    "revision": "f87d790cd7f4c56a0c81",
+    "url": "/popstore/static/js/runtime-main.7c7dadfb.js"
+  }
+]);
